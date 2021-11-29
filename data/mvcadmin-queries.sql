5c0ea1f4c56f07d339039999de21b9f5af98edf4
cahitYes <efeyesiloglu@hotmail.com>
2021-11-29 13:23:59 +0100

    commit

--- data/mvcadmin-queries.sql
+++ data/mvcadmin-queries.sql
@@ -1,38 +1,25 @@
-# Affichez tous les champs de la table thearticle
-SELECT * FROM thearticle;
+select * from thearticle;
+select * from thearticle_has_thesection;
+select * from theright;
+select * from thesection;
+select * from theuser;
+select thearticle.idthearticle, thearticle.thearticletitle, thearticle.thearticleText, thearticle.thearticleDate, thearticle.thearticlestatus, thearticle.theuser_idtheuser, theuser.theusername, theuser.theuserLogin FROM thearticle inner join theuser on thearticle.theuser_idtheuser = theuser.idtheuser;
+select thearticle.idthearticle, thearticle.thearticletitle, thearticle.thearticleText, thearticle.thearticleDate, thearticle.thearticlestatus, thearticle.theuser_idtheuser, thesection.idthesection, thesection.thesectiontitle from thearticle left join thearticle_has_thesection on thearticle.idthearticle = thearticle_has_thesection.thearticle_idthearticle left join thesection on thesection.idthesection = thearticle_has_thesection.thesection_idthesection ;
+select thearticle.*,group_concat(thesection.idthesection) AS idthesection,
+ group_concat(thesection.thesectiontitle SEPARATOR '|||') AS thesectiontitle
+ from thearticle
+ left join thearticle_has_thesection on thearticle.idthearticle = thearticle_has_thesection.thearticle_idthearticle 
+ left join thesection on thesection.idthesection = thearticle_has_thesection.thesection_idthesection 
+ GROUP by thearticle.idthearticle;
+ select thearticle.*,
+ theuser.theusername,theuser.theuserlogin,
+ group_concat(thesection.idthesection) AS idthesection,
+ group_concat(thesection.thesectiontitle SEPARATOR '|||') AS thesectiontitle
+ from thearticle
+ left join thearticle_has_thesection on thearticle.idthearticle = thearticle_has_thesection.thearticle_idthearticle 
+ left join thesection on thesection.idthesection = thearticle_has_thesection.thesection_idthesection 
+ inner join theuser
+ on thearticle.theuser_idtheuser = theuser.idtheuser
+ GROUP by thearticle.idthearticle;
+ 
 
-# Affichez tous les champs de la table thearticle_has_thesection
-SELECT * FROM thearticle_has_thesection;
-
-# Affichez tous les champs de la table theright
-SELECT * FROM theright;
-
-# Affichez tous les champs de la table thesection
-SELECT * FROM thesection;
-
-# Affichez tous les champs de la table theuser
-SELECT * FROM theuser;
-
-# Affichez tous les champs de la table thearticle en y joignant 
-# obligatoirement les champs theuserName et theuserLogin de la table
-# theuser
-SELECT thearticle.*, theuser.theuserName, theuser.theuserLogin
-	FROM thearticle
-    INNER JOIN theuser
-		ON thearticle.theuser_idtheuser = theuser.idtheuser;
-
-# Affichez tous les champs de la table thearticle en y joignant 
-# facultativement les champs idthesection et thesectionTitle de 
-# la table thesection (2 lignes de résultats)
-
-# Affichez tous les champs de la table thearticle en y joignant 
-# facultativement les champs idthesection et thesectionTitle de 
-# la table thesection mais sur une ligne, en concaténant idthesection
-# avec une "," et thesectionTitle avec "|||"
-
-# Affichez tous les champs de la table thearticle en y joignant 
-# obligatoirement les champs theuserName et theuserLogin de la table
-# theuser et
-# facultativement les champs idthesection et thesectionTitle de 
-# la table thesection mais sur une ligne, en concaténant idthesection
-# avec une "," et thesectionTitle avec "|||"
